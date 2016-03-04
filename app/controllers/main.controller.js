6de0d4b3200f0e1d48c07795d59b81a531dc73b5
lexthatch <lexthatch@hotmail.com>
2016-03-03 19:44:25 -0700

    initial commit

--- app/controllers/main.controller.js
+++ app/controllers/main.controller.js
@@ -14,6 +14,45 @@
         $scope.greet = function () {
             var msg = MainService.greet($scope.name)
             $scope.greetings.push(msg)
+
         }
+        var question = {
+            text: "How do you create an IFFE?",
+            author: "Bob",
+            date: new Date(),
+            tags: ["javascript", "function", "encapsulation"]
+            answers: [
+                {
+                    text: "An IFFE is something you create when you don't know much about JavaScript."
+                    author: "Bob",
+                    date: new Date(),
+                    score: -5,
+                    accepted: false,
+                    comments: [
+                        {
+                            text: "That's not right sir...",
+                            author: "Jimmy Jow",
+                            flagged: false,
+                        }
+                    ]
+                }, {
+                    text: "Iffe stands for an immediately Invoked Function Expression"
+                    author: "Chim Chim",
+                    date: new Date(),
+                    score: 10,
+                    accepted: false,
+                    comments: [
+                        {
+                            text: "Awesome! I never knew that",,
+                            author: "Ken Wilcox",
+                            flagged: false,
+                        },{
+                            text: "Why so cheap on the details bub? How about how we create it not wshat it is.",,
+                            author: "Lou-Lou",
+                            flagged: false,
+                        }
+                    
+                    ]
+                }
     }
-})();
+    })();
